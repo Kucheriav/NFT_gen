4454fd48e0ed1ebd8db8c835ebd3162fd0e6c6bd
Kucheriav <kucheriav.borisovich@yandex.ru>
2025-06-16 11:52:59 +0000

    server: finished with migration, removed explicit app key

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, redirect, url_for, session
 from database import Base, engine
 from api.nft import bp as nft_bp
 from api.auth import bp as auth_bp
+from dotenv import load_dotenv
+import os
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(base_dir, '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"
-Base.metadata.create_all(bind=engine)
+app.secret_key =  os.getenv('APP_SECRET_KEY')
+#Base.metadata.create_all(bind=engine)
 app.register_blueprint(nft_bp)
 app.register_blueprint(auth_bp)
 
--- services/create_hash.py
+++ services/create_hash.py
@@ -1,3 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from models.user import User
+from models.nft_image import NFTImage
 from database import SessionLocal
 from models.nft_image import NFTImage
 from services.nft_generator import get_layers_hash
